ab0237b9b939a476c04fe897c7aa1e7fc85ed916
mel-nick <48454124+mel-nick@users.noreply.github.com>
2020-10-07 17:24:15 +0300

    Update server.js
    HTTPS

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const socketio = require('socket.io');
-const http = require('http');
+const https = require('https');// HTTPS://
 const mongoose = require('mongoose');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
